63dae441c875206ee3e526f7b2ea5dd64a3e0c2f
someshdharmalingam <150359578+someshdharmalingam@users.noreply.github.com>
2024-08-04 12:27:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('registerButton').addEventListener('click', function() {
+    window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSe7cPyUusbvfoWZu1WAQeFGYuq_SdTwpyuUuTryrgTTFHiNCQ/viewform?usp=sf_link';
+});
